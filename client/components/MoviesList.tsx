9c9956df2ba942ea8f22662df8e98372269ef8fe
Henry Nguyen <QuyDNN@outlook.com>
2026-06-05 11:28:56 +1200

    WIP feature components with hardcode. Also removed stray boilerplate fruits.ts from server/routes/fruits.ts

--- client/components/MoviesList.tsx
+++ client/components/MoviesList.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Movie } from '../../models/index.js'
 
 function MoviesList() {
-  return <div>MoviesList.</div>
+  const movies: Movie[] = [
+    { id: 1, name: 'Kung Fu Panda', category: 'Family', duration: 92 },
+    { id: 2, name: 'Ready Player One', category: 'Sci-fi', duration: 140 },
+    { id: 3, name: 'Avatar', category: 'Sci-fi', duration: 162 },
+  ]
+  return (
+    <div>
+      <h1>MoviesList</h1>
+    </div>
+  )
 }
 
 export default MoviesList
