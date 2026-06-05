060efee4e5af4c85c1b31f3dc7c38880f6d6eab1
Henry Nguyen <QuyDNN@outlook.com>
2026-06-05 15:23:41 +1200

    removed hardcode from MovieDetails.tsx

--- client/components/MovieDetails.tsx
+++ client/components/MovieDetails.tsx
@@ -35,39 +35,6 @@ function MovieDetails() {
     e.preventDefault()
     mutation.mutate({ name, date, description, movie_id: movieId })
   }
-  // const movie: Movie = {
-  //   id: 3,
-  //   name: 'Avatar',
-  //   category: 'Sci-fi',
-  //   duration: 162,
-  // }
-
-  // // Review array
-  // const reviews: Review[] = [
-  //   {
-  //     id: 4,
-  //     name: 'Maneet',
-  //     date: '6th May 2025',
-  //     movie_id: 3,
-  //     description: 'Meh. Not bad.',
-  //   },
-  //   {
-  //     id: 5,
-  //     name: 'Young',
-  //     date: '3rd May 2026',
-  //     movie_id: 3,
-  //     description:
-  //       "I seriously don't understand why this is the highest grossing film in the world. Their accents are so weird and I don't understand why everyone is blue! What happened to diversity?!!?",
-  //   },
-  //   {
-  //     id: 6,
-  //     name: 'Henry',
-  //     date: '6th January 2025',
-  //     movie_id: 3,
-  //     description:
-  //       "2 stars if I could rate on this site! Don't watch this movie! You will get addicted to it! I've already watched it 10 times in a row...HELP ME!!!",
-  //   },
-  // ]
 
   return (
     <div className="layout">
