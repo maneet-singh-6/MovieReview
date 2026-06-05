4d88efa97544070b8b88514e31c54c540703e6ca
Maneet <maneetsinghbajaj6@gmail.com>
2026-06-05 12:34:44 +1200

    complete routes and server setup

--- server/server.ts
+++ server/server.ts
@@ -9,12 +9,12 @@ server.use(express.json())
 
 server.use('/api/v1/movies', movieRoutes)
 
-if (process.env.NODE_ENV === 'production') {
-  server.use(express.static(Path.resolve('public')))
-  server.use('/assets', express.static(Path.resolve('./dist/assets')))
-  server.get('*', (req, res) => {
-    res.sendFile(Path.resolve('./dist/index.html'))
-  })
-}
+// if (process.env.NODE_ENV === 'production') {
+//   server.use(express.static(Path.resolve('public')))
+//   server.use('/assets', express.static(Path.resolve('./dist/assets')))
+//   server.get('*', (req, res) => {
+//     res.sendFile(Path.resolve('./dist/index.html'))
+//   })
+// }
 
 export default server
