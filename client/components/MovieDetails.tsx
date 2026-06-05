dff8ac34b0fe75b282a7711d40bedaca39b9dc61
Henry Nguyen <QuyDNN@outlook.com>
2026-06-05 12:25:00 +1200

    add MovieDetails with hardcoded movie and reviews. Feature-components ticket done!

--- client/components/MovieDetails.tsx
+++ client/components/MovieDetails.tsx
@@ -1,4 +1,4 @@
-import { Movie } from '../../models/index.js'
+import { Movie, Review } from '../../models/index.js'
 
 function MovieDetails() {
   const movie: Movie = {
@@ -8,9 +8,37 @@ function MovieDetails() {
     duration: 162,
   }
 
+  // Review array
+  const reviews: Review[] = [
+    {
+      id: 4,
+      name: 'Maneet',
+      date: '6th May 2025',
+      movie_id: 3,
+      description: 'Meh. Not bad.',
+    },
+    {
+      id: 5,
+      name: 'Young',
+      date: '3rd May 2026',
+      movie_id: 3,
+      description:
+        "I seriously don't understand why this is the highest grossing film in the world. Their accents are so weird and I don't understand why everyone is blue! What happened to diversity?!!?",
+    },
+    {
+      id: 6,
+      name: 'Henry',
+      date: '6th January 2025',
+      movie_id: 3,
+      description:
+        "2 stars if I could rate on this site! Don't watch this movie! You will get addicted to it! I've already watched it 10 times in a row...HELP ME!!!",
+    },
+  ]
+
   return (
     <div className="layout">
       <h1>Movie Details:</h1>
+      {/* Movie info: */}
       <ul>
         <li>
           {movie.name}
@@ -18,6 +46,18 @@ function MovieDetails() {
           {movie.duration}
         </li>
       </ul>
+
+      {/* reviews */}
+      <div>
+        {reviews &&
+          reviews.map((review) => (
+            <li key={review.id}>
+              {review.name}
+              {review.date}
+              {review.description}
+            </li>
+          ))}
+      </div>
     </div>
   )
 }
