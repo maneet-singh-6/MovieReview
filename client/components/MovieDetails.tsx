204ab5472208a7d175b24faebce5bde61ee5b58e
Henry Nguyen <QuyDNN@outlook.com>
2026-06-05 12:14:22 +1200

    WIP - add hardcoded Avatar from seed data

--- client/components/MovieDetails.tsx
+++ client/components/MovieDetails.tsx
@@ -1,5 +1,25 @@
+import { Movie } from '../../models/index.js'
+
 function MovieDetails() {
-  return <div>MovieDetails</div>
+  const movie: Movie = {
+    id: 3,
+    name: 'Avatar',
+    category: 'Sci-fi',
+    duration: 162,
+  }
+
+  return (
+    <div className="layout">
+      <h1>Movie Details:</h1>
+      <ul>
+        <li>
+          {movie.name}
+          {movie.category}
+          {movie.duration}
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default MovieDetails
